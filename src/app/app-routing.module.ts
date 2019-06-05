a23a9cc19b8a5bcd319cf5a258a2b1036555e011
Conrado Zapata <excz010715@sri.ad>
2019-06-05 16:50:39 -0500

    Primera versión  de gestor de finanzas personales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { Page404Component } from './shared/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'gastos', component: GastosComponent },
+  { path: '**', component: Page404Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabeceraComponent } from './shared/cabecera/cabecera.component';
+import { ControlesComponent } from './shared/controles/controles.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { Page404Component } from './shared/page404/page404.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    ControlesComponent,
+    GastosComponent,
+    DashboardComponent,
+    FooterComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
