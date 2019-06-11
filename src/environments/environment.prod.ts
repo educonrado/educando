869c493d8e6c60b9e908a69fa87bfc10b429dfcc
Conrado Zapata <excz010715@sri.ad>
2019-06-11 17:03:55 -0500

    Funcionalidad crear gastos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuC2WwcW8QhsGBS5UY93kR293swrSvZ1Y",
+    authDomain: "finanzas-c0350.firebaseapp.com",
+    databaseURL: "https://finanzas-c0350.firebaseio.com",
+    projectId: "finanzas-c0350",
+    storageBucket: "finanzas-c0350.appspot.com",
+    messagingSenderId: "513241450731",
+    appId: "1:513241450731:web:2d32d86af9b92bc2"
+  }
 };
