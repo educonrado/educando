4cbe8f94f88ec294f273d9056da01eab9888d767
excz <excz.test@gmail.com>
2019-06-11 22:36:20 -0500

    Dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { Gasto } from './../../model/gasto';
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../../services/firebase.service';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public gastos: Gasto[] = [];
+  public totalGastos: number = 0;
+  constructor(
+    private _firebaseService: FirebaseService
+  ) {
+   }
 
   ngOnInit() {
+    this.cargarGastos();
+    this.calcularGastoTotal();
+  }
+
+  cargarGastos(){
+    this._firebaseService.obtenerGastos().subscribe(data=>{
+      this.gastos = data.map(e=>{
+        return {
+          $key: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Gasto;
+      })
+    });
+  }
+
+  calcularGastoTotal(){
+    /*for (let index = 0; index < this.gastos.length; index++) {
+      const element = this.gastos[index];
+      this.totalGastos += element.valor;
+      console.log(this.totalGastos);
+    }*/
+    this.gastos.forEach(element => {
+      this.totalGastos=this.totalGastos+element.valor;
+    });
   }
 
 }
